453b4cf62f1c03b0911602cea608a59043ffafbb
Sam Catley <scp94373@gmail.com>
2020-07-04 11:19:45 +0100

    changing readme

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## This app is for empowering self belief and restoring faith in oneself.
 
-### This app was made with purely good intentions and should not be used for any negative or selfish reasons.
+### This app was made with purely good intentions and should not be used for any negative or destructive agendas.
 
 Please create issue tickets for empowering affirmations you wish to be added.
 Please create issue tickets for any improvements you wish for me to make or create pull requests.
--- src/Affirmations/Conscious/PowerAffirmations/PowerAffirmations.js
+++ src/Affirmations/Conscious/PowerAffirmations/PowerAffirmations.js
@@ -5,10 +5,19 @@ class PowerAffirmations extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            affirmations: [
+                "I am a positive person",
+                "",
+                "I have many unique skills",
+                "",
+                "I will use these skills effectively"
+
+        ],
             words: ["I", "am", "a", "positive", "person", "",
                     "I", "have", "many", "unique", "skills", "",
                     "I", "will", "use", "these", "skills", "effectively", "",
-                    "I", "look", "at", "all", "situations", "in", "a", "positive", "light", ""]
+                    "I", "look", "at", "all", "situations", "in", "a", "positive", "light", ""
+                ]
           }
     }
     
@@ -25,9 +34,21 @@ class PowerAffirmations extends Component {
         //         }
         //     }
         // }
-        const words = this.state.words.map((word) => {
-            return <span key={word}>{word}</span>;
+        const words = this.state.words.map((word, i) => {
+            console.log(word)
+            return <span key={i}>{word}</span>;
            });
+
+        // console.log(this.state.affirmations[0].affirmation.split(" "))
+        //    const words = this.state.affirmations.map((affirmation, i) => {
+        //     const stringWithNoSpaces = affirmation.split(" ");
+        //     // console.log("stringWithNoSpaces: ", stringWithNoSpaces, "type of: ", stringWithNoSpaces.typeOf)
+        //     stringWithNoSpaces.map((word, i) => {
+        //         console.log("for each word: ", word)
+        //         return <span key={i}>{word}</span>;
+        //     })
+            
+        //    });
         return (
             <div>
                 <h1>Empowering Affirmations</h1>
